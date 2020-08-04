3b3f62ffa6ab2d25eb9f94ae9dc17676872512ca
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-08-04 13:24:09 +0530

    Update DHT11 Sensor.py

--- DHT11 Sensor.py
+++ DHT11 Sensor.py
@@ -7,3 +7,4 @@ GPIO.setwarnings(False)
 GPIO.setup (4,GPIO.OUT)
 while True:
    Humidity,temperature = Adafruit_DHT.read_retry (11,4)
+ print (‘Temp: (0:0.1F) C Humidity:(1:0.1F)%’,format(temperature,humidity))

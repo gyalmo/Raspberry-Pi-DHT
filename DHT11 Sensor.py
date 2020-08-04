6adc24d273ab0b3e3966065813b895a9e2096405
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-08-04 13:23:38 +0530

    Update DHT11 Sensor.py

--- DHT11 Sensor.py
+++ DHT11 Sensor.py
@@ -1,1 +1,9 @@
 
+import RPi.GPIO  as GPIO
+Import Adafruit_DHT
+import time as t
+GPIO.setmode (GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup (4,GPIO.OUT)
+while True:
+   Humidity,temperature = Adafruit_DHT.read_retry (11,4)
